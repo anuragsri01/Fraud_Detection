3c76ab2389f8ecd3c88b2ee6bbb371a91c315e88
Anurag Srivastava <anurag.anu156@gmail.com>
2020-01-08 12:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
+import pandad as pd
 from flask import Flask, request ,jsonify
 import random
+import pickle
 import requests
 
+#load model
+model = pickle.load(open('finalized_model.sav', 'rb'))
+
+#app
 app = Flask(__name__)
 
-@app.route('/data', methods=['POST'])
-def verify():
-    data = request.get_json()
-    print(data)
-    return jsonify(data)
+#routes
+@app.route('/', methods=['POST'])
 
-
-@app.route('/detect', methods=['GET'])
-def respond():
-    req = request.get_json()
-    print(req)
-    return "you have reach to root endpoint"
+def predict():
+    #get data
+    data = request.get_json(force=True)
+    
+    #convert data into dataframe
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
+    
+    #predictions
+    result = model.predict(data_df)
+    
+    #send back to browser
+    output = {'results': int(result[0])}
+    
+    #return data
+    return jsonify(results=output)
 
 if __name__ == '__main__':
-    modelfile = 'finalized_model.sav'
-    model = p.load(open(modelfile, 'rb'))
+#     modelfile = 'finalized_model.sav'
+#     model = p.load(open(modelfile, 'rb'))
     app.run()
