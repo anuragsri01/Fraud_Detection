9b4722bea2ca3bbbf42cc4294340bb9efbb71464
Anurag Srivastava <anurag.anu156@gmail.com>
2020-01-09 12:07:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,28 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
-def verify():
-    data = request.get_json()
+@app.route('/input/', methods=['GET'])
+def respond():
+    req = request.args.get("input", None)
+    print(f"got input{req}")
+
+    response = {}
+    return jsonify(req)
+
+
+@app.route('/predict/', methods=['POST'])
+def predict():
+    data = request.get_json('req')
     print(data)
     return jsonify(data)
 
 
-@app.route('/', methods=['GET'])
-def respond():
-    req = request.args
-    print(req)
-    return jsonify(req)
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
 
 if __name__ == '__main__':
     model_file = 'finalized_model.sav'
     model = p.load(open(model_file, 'rb'))
-    app.run()
+    app.run(threaded=True, port=5000)
